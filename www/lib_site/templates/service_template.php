c0278fb5d29948121ea972746827c3a4d6f82fc0
spiraspera <anton.kudashov@gmail.com>
2016-06-17 19:28:26 +0300

    Office and service changes

--- www/lib_site/templates/service_template.php
+++ www/lib_site/templates/service_template.php
@@ -1,27 +1,69 @@
+<?
+
+	/* populate reverse array for services in office centers: [service_id] => service_in_office_data */
+
+	if (isset($_DATA['office_center2service_group'])) {
+		
+		foreach($_DATA['office_center2service_group']['items'] as &$service_group) {
+			
+			$service_groups_reverse[$service_group['service_group_id']] = $service_group;
+			
+		}
+		
+		unset($service_group);
+		
+	}
+
+	/* prepare some needed data */
+	
+	$vd_service_in_center_id = $_GET['service'];
+	
+	$vd_service_in_center_title = $service_groups_reverse[$vd_service_in_center_id]['service_group_id_lookup'];
+	
+	$vd_service_in_center_office_title = $service_groups_reverse[$vd_service_in_center_id]['office_center_id_lookup'];
+
+	$vd_service_in_center_price = $service_groups_reverse[$vd_service_in_center_id]['price'];
+	
+	$vd_service_in_center_price = preg_replace('/( |&nbsp;)(.*)( |&nbsp;)/', ' <span>$2</span> ', $vd_service_in_center_price);
+	
+	$vd_service_group_id_css = $_DATA['service_group']['items'][$vd_service_in_center_id]['css_signature'];
+	
+?>
+
 <div class="vd_serviceincenter_wrapper">
-	<div class="vd_serviceincenter_wrapper-header office">
-		<div class="vd_serviceincenter_wrapper-header-price">
-			от <span>13 490</span> рублей в месяц
-		</div>
-		<div class="vd_serviceincenter_wrapper-header-servicename">
-			Аренда офисов
-		</div>
-		<div class="vd_serviceincenter_wrapper-header-centername">
-			в бизнес-центре «Арма»
-		</div>
+	<div class="vd_serviceincenter_wrapper-header <? echo $vd_service_group_id_css; ?>">
+		<div class="g-container">
+			<div class="g-container-row">
+				<div class="vd_serviceincenter_wrapper-header-price">
+					<? echo $vd_service_in_center_price; ?>
+				</div>
+				<div class="vd_serviceincenter_wrapper-header-logo"></div>
+				<div class="vd_serviceincenter_wrapper-header-servicename">
+					<? echo $vd_service_in_center_title; ?>
+				</div>
+				<div class="vd_serviceincenter_wrapper-header-centername">
+					в бизнес-центре «<? echo $vd_service_in_center_office_title; ?>»
+				</div>
+			</div>
+		</div>		
 	</div>
 	<div class="vd_serviceincenter_wrapper-menu">
-		<ul>
-			<li><a href="#about">Об услуге</a></li>
-			<li><a href="#gallery">Галерея</a></li>
-			<li><a href="#layout">План офисов</a></li>
-			<li><a href="#freeoffices">Свободные офисы бизнес-центра</a></li>
-			<li><a href="#special">Спецпредложения</a></li>
-		</ul>
+		<div class="g-container">
+			<div class="g-container-row">
+				<ul>
+					<li><a href="#about">Об услуге</a></li>
+					<li><a href="#gallery">Галерея</a></li>
+					<li><a href="#layout">План офисов</a></li>
+					<li><a href="#freeoffices">Свободные офисы бизнес-центра</a></li>
+					<li><a href="#special">Спецпредложения</a></li>
+					<li class="helper"></li>
+				</ul>
+			</div>
+		</div>		
 	</div>
 	<div class="vd_serviceincenter_wrapper-about">
 		<a name="about" /></a>
-		<div class="vd_serviceincenter_wrapper-about-title">Об услуге</div>
+		<h2 class="g-section-title">Об услуге</h2>
 		<div class="vd_serviceincenter_wrapper-about-list">
 			<div class="vd_serviceincenter_wrapper-about-list-sublist included">
 				<div class="vd_serviceincenter_wrapper-about-list-sublist-header">
@@ -92,9 +134,7 @@
 	</div>
 	<div class="vd_serviceincenter_wrapper-gallery">
 		<a name="gallery" /></a>
-		<div class="vd_serviceincenter_wrapper-gallery-title">
-			Галерея
-		</div>
+		<h2 class="g-section-title">Галерея</h2>
 		<div class="cycle-slideshow"
 			data-cycle-timeout="0"
 			data-cycle-fx="carousel"
@@ -111,7 +151,7 @@
 	</div>
 	<div class="vd_serviceincenter_wrapper-layout">
 		<a name="layout" /></a>
-		<div class="vd_serviceincenter_wrapper-layout-title">План офисов</div>
+		<h2 class="g-section-title">План офисов</h2>
 		<div class="vd_serviceincenter_wrapper-layout-image">
 			<img src="/images/fake_office_layout.png" />
 			<div class="vd_serviceincenter_wrapper-layout-image-zoom">
@@ -221,7 +261,7 @@
 	</div>
 	<div class="vd_serviceincenter_wrapper-special">
 		<a name="special" /></a>
-		<div class="vd_serviceincenter_wrapper-special-title">Cпецпредложения</div>
+		<h2 class="g-section-title">Cпецпредложения</h2>
 		<div class="vd_serviceincenter_wrapper-special-list">
 			<div class="vd_serviceincenter_wrapper-special-list-item office">
 				<div class="vd_serviceincenter_wrapper-special-list-item-text"><!--
