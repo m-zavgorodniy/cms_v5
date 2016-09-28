9308c85c7a7f7ef300dab52bc891a49631dc836c
maxim <maxim.zavgorodniy@semestry.com>
2016-09-28 19:39:44 +0100

    404

--- www/lib_site/templates/404.php
+++ www/lib_site/templates/404.php
@@ -1,18 +1,57 @@
-<div class="body-content-main">
-	<h1>Ошибка 404</h1>
-    <h3>Неправильно набран адрес или такой страницы больше не существует.<br />Возможные причины возникновения этой ошибки:</h3>
-    <p>
-    	<br />
-    	<strong>Неправильно указан адрес страницы</strong>
-        <br />
-        Проверьте правильность набора адресной страницы в адресной строке браузера
-    </p>
-    <p>
-    	<strong>Эта страница была удалена с сервера либо перемещена по другому адресу</strong>
-    	<br />
-        Попробуйте найти интересующий Вас документ, используя навигацию по разделам сайта
-    </p>
-    <p>&nbsp;</p>
-    <p><a href="/">Перейти на главную страницу</a></p>
-    <p align="right"><img src="/images/404.jpg" alt="" /></p>
-</div>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>404 - Деловой</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="format-detection" content="telephone=no" />
+	<link rel="stylesheet" type="text/css" href="/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="/css/content.css" />
+    <link rel="stylesheet" type="text/css" href="/css/delovoy_styles.css" />
+    <link rel="stylesheet" type="text/css" href="/css/delovoy_styles_mobile.css" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <style>
+        .text-content h1 {font-size: 40px;}
+        .bin {text-align: right;}
+    </style>
+</head>
+<body>
+    <div class="body-wrap">
+        <header class="header">
+            <div class="header-top">
+            </div>
+            <div class="g-container"><div class="g-container-row">
+                <div class="header-nav g-clearfix">
+                    <div class="header-nav-logo">
+                    <a href="/">
+                        <img src="/images/svg/logo_ru.svg" alt="Деловой - сеть офисных центров">
+                        <span class="header-nav-logo-after">Деловой</span>
+                    </a>
+                    </div>
+                </div>
+             </div></div>
+        </header>
+        <div class="g-container"><div class="g-container-row">
+            <div class="body-content-main text-content">
+                <h1>Ошибка 404</h1>
+                <h3>Неправильно набран адрес или такой страницы больше не существует.<br />Возможные причины возникновения этой ошибки:</h3>
+                <p>
+                    <br />
+                    <strong>Неправильно указан адрес страницы</strong>
+                    <br />
+                    Проверьте правильность набора адресной страницы в адресной строке браузера
+                </p>
+                <p>
+                    <strong>Эта страница была удалена с сервера либо перемещена по другому адресу</strong>
+                    <br />
+                    Попробуйте найти интересующий Вас документ, используя навигацию по разделам сайта
+                </p>
+                <p>&nbsp;</p>
+                <p><a href="/">Перейти на главную страницу</a></p>
+                <p class="bin"><img src="/images/404.jpg" alt="" /></p>
+            </div>
+        </div></div>
+    </div>
+</body>
+</html>
